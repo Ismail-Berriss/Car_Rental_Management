1fe74e77c0ce85d45a8cedb98a054399ec6cb9e9
ismail berriss <ismailberriss@gmail.com>
2023-05-21 16:17:09 +0100

    Adding the Update Car method

--- public/app/controllers/controllerCars.js
+++ public/app/controllers/controllerCars.js
@@ -6,46 +6,45 @@ app.controller("controllerCar", function($scope, $firebaseArray) {
     //  Setting the views
     $scope.showCarsReadView = true;
     $scope.showCarsCreateView = false;
-    // $scope.success = false;
+    $scope.showCarsUpdateView = false;
 
     $scope.showCarsRead = function() {
         $scope.showCarsReadView = true;
         $scope.showCarsCreateView = false;
+        $scope.showCarsUpdateView = false;
         $scope.cars = Car;
     }
 
     $scope.showCarsCreate = function() {
         $scope.showCarsReadView = false;
         $scope.showCarsCreateView = true;
+        $scope.showCarsUpdateView = false;
     }
 
-    /* Read */
-    $scope.cars = Car;
-    /* Car.$ref().once('value', function(snap) {
-        angular.forEach(snap.val(), function(index) {
-            console.log(index.$key);
-        });
-    }); */
-    
-    
+    $scope.showCarsUpdate = function(car) {
+        $scope.showCarsReadView = false;
+        $scope.showCarsCreateView = false;
+        $scope.showCarsUpdateView = true;
 
+        $scope.car = car;
+    }
 
     /* Create */
     $scope.createCar = function () {
 
-        $scope.newCar.numero = 1;
+        $scope.newCar.id = 1;
 
         // Car ID, lastest car's ID + 1
         Car.$ref().once('value', function(snap) {
             angular.forEach(snap.val(), function(index) {
                 $scope.cars = index;
-                $scope.newCar.numero = index.numero + 1;
+                $scope.newCar.id = index.id + 1;
             });
         });
 
         // Adding the new car
         Car.$add({
-            numero: $scope.newCar.numero,
+            id: $scope.newCar.id,
             type: $scope.newCar.type,
             marque: $scope.newCar.marque,
             modele: $scope.newCar.modele,
@@ -57,6 +56,20 @@ app.controller("controllerCar", function($scope, $firebaseArray) {
         // Clearing the input fields
         $scope.newCar = {};
         
+    };
+
+    /* Read */
+    $scope.cars = Car;
+    /* Car.$ref().once('value', function(snap) {
+        angular.forEach(snap.val(), function(index) {
+            console.log(index.$key);
+        });
+    }); */
+
+    /* Update */
+    $scope.updateCar = function(car) {
+        Car.$save(car);
+        $scope.showCarsRead();
     };
 
     /* Delete */
